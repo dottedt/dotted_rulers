bf0eac1ddb03b3b034983a404ec23b2adf8ac194
Scott <scott@dottedt.com>
2014-03-26 14:06:43 -0500

    methods for subtract, multiply and divide

--- lib/dotted_rulers/array.rb
+++ lib/dotted_rulers/array.rb
@@ -1,5 +1,19 @@
 class Array
+
   def sum(start = 0)
     inject(start, &:+)
   end
+
+  def subtract(start = 0)
+    inject(start, &:-)
+  end
+
+  def multiply(start = 0)
+    inject(start, &:*)
+  end
+
+  def divide(start = 0)
+    inject(start, &:/)
+  end
+
 end
