85e05383808916b61e8e6ea82c57d5c9f102dba4
Scott <scott@dottedt.com>
2014-03-26 12:31:01 -0500

    Changes to use rack

--- lib/dotted_rulers.rb
+++ lib/dotted_rulers.rb
@@ -1,5 +1,10 @@
 require "dotted_rulers/version"
 
 module DottedRulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello to Scott from Ruby on Rulers!"]]
+    end
+  end
 end
