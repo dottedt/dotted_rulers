eb495020b34c2e587ba839e4e82a2e5da7d71b19
Scott <scott@dottedt.com>
2014-03-26 13:59:28 -0500

    Adding array. Example of how gem looks at git while building

--- lib/dotted_rulers.rb
+++ lib/dotted_rulers.rb
@@ -1,8 +1,10 @@
+require "array"
 require "dotted_rulers/version"
 
 module DottedRulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
         ["Hello to Scott from Ruby on Rulers!"]]
     end
